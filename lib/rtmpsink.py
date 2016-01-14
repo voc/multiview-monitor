685c304f97a55a1a84e90ac17cf776fac33cd5d5
MaZderMind <github@mazdermind.de>
2016-01-14 14:55:07 +0100

    rtmp sink

--- lib/rtmpsink.py
+++ lib/rtmpsink.py
@@ -7,3 +7,29 @@ from lib.config import Config
 class RtmpSink(object):
 	def __init__(self):
 		self.log = logging.getLogger('RtmpSink')
+
+		# intervideosrc -> encoder -> muxer -> rtmp
+		pipeline = """
+			flvmux streamable=true name=mux !
+				rtmpsink location="{url}"
+
+			intervideosrc channel=out !
+				videoconvert !
+				x264enc !
+				queue !
+				mux.
+
+			audiotestsrc wave=silence !
+				audio/x-raw,channels=2,rate=44100 !
+				faac !
+				aacparse !
+				queue !
+				mux.
+		""".format(
+			caps=Config.get('output', 'caps'),
+			url=Config.get('output', 'rtmp')
+		)
+
+		self.log.debug('Starting Sink-Pipeline:\n%s', pipeline)
+		self.pipeline = Gst.parse_launch(pipeline)
+		self.pipeline.set_state(Gst.State.PLAYING)
