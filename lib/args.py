726a8f69a66ccac6112f716d9fb084d5fd02ab83
MaZderMind <github@mazdermind.de>
2016-01-15 13:20:52 +0100

    configuration for universal setup

--- lib/args.py
+++ lib/args.py
@@ -10,7 +10,7 @@ parser.add_argument('-v', '--verbose', action='count', default=0,
 parser.add_argument('-c', '--color', action='store', choices=['auto', 'always', 'never'], default='auto',
 	help="Control the use of colors in the Log-Output")
 
-parser.add_argument('-i', '--ini-file', action='store',
+parser.add_argument('-i', '--ini-file', action='store', required=True,
 	help="Load a custom config.ini-File")
 
 
